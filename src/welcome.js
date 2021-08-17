f6ce35a0d05ee3dafb441ef1a7625409c47cbd30
Shane McDonald <mcdonaldshane33@gmail.com>
2021-08-17 18:17:21 -0500

    set up so far

---
+++ src/welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WelcomePage extends Component {
+    render() {
+        return {
+
+        }
+    }
+}
+
+export default WelcomePage
+        
