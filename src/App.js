0680d136faee263a9d23a0ad08954ce20a300f63
Shane McDonald <mcdonaldshane33@gmail.com>
2021-08-17 19:44:46 -0500

    welcome page set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 
 import WelcomePage from './welcome.js'
+import store from './store.js'
+import { Provider } from 'react-redux';
+
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>Page not found: {location.pathname}</h3>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome Shopper</h1>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            <Link to='/'>We Are Servants To Commerce</Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    <Switch>
+    <Route exact path='/'component={WelcomePage}/>
+    <Route path='/clothes' component={Clothes}/>
+    <Route path='/shoes' component={Shoes}/>
+    <Route path='/tech' component={Tech}/>
+    <Route component={NoMatch}/>
+  </Switch>
+  </BrowserRouter>
+  </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme.js';
+
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
